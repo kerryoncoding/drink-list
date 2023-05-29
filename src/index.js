e0b685932799a7406ab9e8a3196d7a3531a43b0f
Kerry Guarino <kerry.guarino@gmail.com>
2023-05-29 15:14:52 -0400

    added drink info (name, instructions, photo) to ul

--- src/index.js
+++ src/index.js
@@ -32,13 +32,23 @@ function addLinksToList(){
 
 function getDrinkDetails(element){
    element.addEventListener("click", () => {
-   document.getElementById("drinkList").innerHTML = ""
+   ul = document.getElementById("drinkList")
+   ul.innerHTML = ""
+
    console.log(element)
    console.log(element.dataset.id)
    fetch(url + `/lookup.php?i=${element.dataset.id}`)
       .then((resp)=>resp.json())
       .then((data)=> {
-              console.log(data.drinks[0].strDrink)
+
+         ul.innerHTML = `
+         <h2>Drink: ${data.drinks[0].strDrink}</h2>
+
+         <p>Instructions: ${data.drinks[0].strInstructions}</p>
+         <p><img class="picture" src="${data.drinks[0].strDrinkThumb}" alt="image of ${data.drinks[0].strDrink}">
+         `
+   
+           //   console.log(data.drinks[0].strDrink)
               console.log(data.drinks[0].strIngredient1)
               console.log(data.drinks[0].strIngredient15)
               console.log(data.drinks[0].strInstructions)
