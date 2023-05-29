f2ff889494c3509aa405e393538c3910213339ab
Kerry Guarino <kerry.guarino@gmail.com>
2023-05-29 10:33:26 -0400

    successful GET fetch request

--- src/index.js
+++ src/index.js
@@ -5,13 +5,18 @@ document.addEventListener("DOMContentLoaded", () => {
    getDrinkList()
 })
 
+let url = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic"
+
 function getDrinkList(){
    //make the api fetch GET request
+   fetch(url)
+   .then((resp)=>resp.json())
+   .then((json)=>console.log(json))
    displayDrinkList()
 }
 
 
 function displayDrinkList(){
    //populate ul with li's containing drink names
-   
+
 }
