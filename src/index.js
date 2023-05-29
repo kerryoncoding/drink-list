0632740f53e1e30fe0c30f3639510086576a36d9
Kerry Guarino <kerry.guarino@gmail.com>
2023-05-29 10:03:06 -0400

    pseudocode for popluating drink list

--- src/index.js
+++ src/index.js
@@ -2,4 +2,16 @@
 document.addEventListener("DOMContentLoaded", () => {
    let showDrinks = document.getElementById("allDrinksBtn")
    showDrinks.addEventListener("click", () => {console.log("I have been clicked")})
+   getDrinkList()
 })
+
+function getDrinkList(){
+   //make the api fetch GET request
+   displayDrinkList()
+}
+
+
+function displayDrinkList(){
+   //populate ul with li's containing drink names
+   
+}
