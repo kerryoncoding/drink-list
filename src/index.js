d8195b706dd4a4080f6b167f54cc20d66de64ad1
Kerry Guarino <kerry.guarino@gmail.com>
2023-05-29 09:57:05 -0400

    connected JS file. Added placeholder event listener

---
+++ src/index.js
@@ -0,0 +1,5 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+   let showDrinks = document.getElementById("allDrinksBtn")
+   showDrinks.addEventListener("click", () => {console.log("I have been clicked")})
+})
