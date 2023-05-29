207d0cd4b863f22e2128e8df507a3f910c0be86d
Kerry Guarino <kerry.guarino@gmail.com>
2023-05-29 14:16:26 -0400

    exploring GET fetch on drink details

--- src/index.js
+++ src/index.js
@@ -5,22 +5,34 @@ document.addEventListener("DOMContentLoaded", () => {
    getDrinkList()
 })
 
-let url = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic"
+let url = "https://www.thecocktaildb.com/api/json/v1/1"
 
 function getDrinkList(){
    //make the api fetch GET request
    const ul = document.getElementById("drinkList")
-   fetch(url)
+   fetch(url + `/filter.php?a=Non_Alcoholic`)
    .then((resp)=>resp.json())
    .then((json)=> {
       (json.drinks).forEach(element => {
         // console.log(element.idDrink)
          ul.innerHTML += `<li><a href="#" data-id="${element.idDrink}" class="drinkName">${element.strDrink}</a></li>`
     })
-   const linkArry = document.querySelectorAll(".drinkName")
-   //linkArry.forEach((e)=> console.log(e.dataset.id))
-  // console.log($(this).data("id"))
-   linkArry.forEach(element => element.addEventListener("click", ()=> console.log(element.dataset.id)))
-   })
+    const linkArry = document.querySelectorAll(".drinkName")
+    //linkArry.forEach((e)=> console.log(e.dataset.id))
+    // console.log($(this).data("id"))
+    linkArry.forEach(element => element.addEventListener("click", ()=> {
+      console.log(element.dataset.id)
+      fetch(url + `/lookup.php?i=${element.dataset.id}`)
+      .then((resp)=>resp.json())
+      .then((data)=> {
+         console.log(data.drinks[0].strDrink)
+         console.log(data.drinks[0].strDrinkThumb)
+         console.log(data.drinks[0].strInstructions)
+
+      })
+   }
+   ))
+})
 }
+
 
