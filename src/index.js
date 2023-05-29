4cab32ac3df54d6c81f8c616b9330add7dcf31ef
Kerry Guarino <kerry.guarino@gmail.com>
2023-05-29 19:55:15 -0400

    replaced let with const.  Removed console logs and notes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-//first event listener - DOMContentLoaded
+
 document.addEventListener("DOMContentLoaded", () => {
-   let showDrinks = document.getElementById("allDrinksBtn")
-   //second event listener - CLICK
+   const showDrinks = document.getElementById("allDrinksBtn")
    showDrinks.addEventListener("click", () => {getDrinkList()})
    getDrinkList()
 })
 
-let url = "https://www.thecocktaildb.com/api/json/v1/1/"
 
+const url = "https://www.thecocktaildb.com/api/json/v1/1/"
 
 
 function getDrinkList(){
-   //make the api fetch GET request
    const ul = document.getElementById("drinkList")
    ul.innerHTML = ""
    const div = document.getElementById("singleDrink")
@@ -20,33 +18,29 @@ function getDrinkList(){
    .then((resp)=>resp.json())
    .then((json)=> {
       (json.drinks).forEach(element => {
-        // console.log(element.idDrink)
          ul.innerHTML += `<li><a href="#" data-id="${element.idDrink}" class="drinkName">${element.strDrink}</a></li>`
-    })
-    addLinksToList()
-})
+      })
+      addLinksToList()
+   })
 }
 
 
 function addLinksToList(){
    const linkArry = document.querySelectorAll(".drinkName")
-   //linkArry.forEach((e)=> console.log(e.dataset.id))
-   // console.log($(this).data("id"))
    linkArry.forEach(element => getDrinkDetails(element))
   }
 
+
 function getDrinkDetails(element){
-   //another click event listener
+
    element.addEventListener("click", () => {
       const ul = document.getElementById("drinkList")
       ul.innerHTML = ""
       const div = document.getElementById("singleDrink")
-      console.log(element)
-      console.log(element.dataset.id)
-   fetch(url + `lookup.php?i=${element.dataset.id}`)
+      fetch(url + `lookup.php?i=${element.dataset.id}`)
       .then((resp)=>resp.json())
       .then((data)=> {
-         let ingredients = getIngrdientsList(data)
+         const ingredients = getIngrdientsList(data)
          div.innerHTML = `
          <h2>${data.drinks[0].strDrink}</h2>
          <p><strong>Ingredients: </strong></p>
@@ -56,28 +50,24 @@ function getDrinkDetails(element){
          <img class="picture" src="${data.drinks[0].strDrinkThumb}" alt="image of ${data.drinks[0].strDrink}">
          <div id="glass"></div>
          `
-
-         let image = document.querySelector('img')
-         let glassType = document.getElementById("glass")
+         const image = document.querySelector('img')
+         const glassType = document.getElementById("glass")
          image.addEventListener("mouseover", ()=> {
             glassType.innerHTML = 
             `<h3>Serve in a ${data.drinks[0].strGlass}.</h3>`
          })
-         let image2 = document.querySelector('img')
+         const image2 = document.querySelector('img')
          image2.addEventListener("mouseout", ()=> {
             glassType.innerHTML = ``
          })   
-            //   console.log(data.drinks[0].strIngredient1)
-            //   console.log(data.drinks[0].strIngredient15)
       })
-      
    })
 }
+
 
 function getIngrdientsList(data){
    let foo = data.drinks[0]
    let ingredientArr = [foo.strIngredient1, foo.strIngredient2, foo.strIngredient3, foo.strIngredient4, foo.strIngredient5, foo.strIngredient6, foo.strIngredient7, foo.strIngredient8, foo.strIngredient9, foo.strIngredient10, foo.strIngredient11, foo.strIngredient12, foo.strIngredient13, foo.strIngredient14, foo.strIngredient15]
- //  console.log(ingredientArr)
    let ingredientShortArr = []
    ingredientArr.forEach((element)=> {
       if (element != null){
